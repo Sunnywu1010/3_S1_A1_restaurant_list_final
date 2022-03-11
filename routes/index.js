1191d5a90703016c8f08ec6bce236987d23abc98
Sunny <wuszuyu791010@gmail.com>
2022-03-11 15:50:38 +1000

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const home = require("./modules/home");
+const users = require("./modules/users"); 
 const restaurants = require("./modules/restaurants");
 
 
 router.use("/", home);
 router.use("/restaurants", restaurants);
-
+router.use("/users", users); 
 
 module.exports = router;
