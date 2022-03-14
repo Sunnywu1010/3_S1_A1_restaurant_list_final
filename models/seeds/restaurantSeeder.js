647306d993bb846783be1ce51a80507c46890fa5
Sunny <wuszuyu791010@gmail.com>
2022-03-15 09:45:02 +1000

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,12 +1,46 @@
-const db=require('../../config/mongoose')
-const Restaurant = require("../restaurant");
-const restaurantList = require("../../restaurant.json").results;
+const bcrypt = require('bcryptjs')
+const User = require('../user')
+const Restaurant = require('../restaurant')
+const db = require('../../config/mongoose')
 
-db.once("open", () => {
-  Restaurant.create(restaurantList)
-    .then(() => {
-      console.log("restaurantSeeder done!");
-      db.close();
+const UserList = require('./user.json')
+const RestaurantList = require('./restaurant.json')
+
+
+db.once('open', () => {
+    UserList.forEach(item => {
+        User.findOne({ email: item.email })
+            .then(user => {
+                if (!user) {
+                    return bcrypt.genSalt(10)
+                        .then(salt => bcrypt.hash(item.password, salt))
+                        .then(hash => User.create({
+                            email: item.email,
+                            password: hash
+                        }))
+                        .catch(err => console.log(err))
+                }
+                return user
+            })
+            .then(user => {
+                item.restaurants.forEach(restaurantId => {
+                    const restaurant = RestaurantList.results.find(restaurant => restaurant.id === restaurantId)
+                    Restaurant.create({
+                        name: restaurant.name,
+                        name_en: restaurant.name_en,
+                        category: restaurant.category,
+                        image: restaurant.image,
+                        location: restaurant.location,
+                        phone: restaurant.phone,
+                        google_map: restaurant.google_map,
+                        rating: restaurant.rating,
+                        description: restaurant.description,
+                        userId: user._id
+                    })
+                })
+            })
     })
-    .catch((err) => console.log(err));
-});
+    db.close()
+    console.log('done')
+    process.exit()
+  })
